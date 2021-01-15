223ac2c979819510cd7c81656dee2717214c9dde
SindhujaCAI002 <sindhuja.n@credavenue.com>
2021-01-15 11:03:04 +0530

    sign up page finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_104910) do
+ActiveRecord::Schema.define(version: 2021_01_13_140620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_104910) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "test for invalid signup" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name: "",
+                                        email: "si@g",
+                                        password: "hey",
+                                        password_confirmation: "hi"}}
+      end
+      assert_template 'users/new'
+     # assert_select 'div#<CSS id for error explanation>'
+      #assert_select 'div.<CSS class for field with errors'
+    end
+      test "test for valid signup" do
+        get signup_path
+        assert_difference 'User.count', 1 do
+          post users_path, params: {user: {name: "hi",
+                                            email: "hi@gmail.com",
+                                            password: "sindhu123",
+                                            password_confirmation: "sindhu123"}}
+          end
+          follow_redirect!
+          assert_template 'users/show'
+          #assert_not flash.FILL_IN
+    end
+end
